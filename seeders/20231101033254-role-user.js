4303fb5f4923a6b06989bc6ddbea30e836068a30
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-11-01 11:00:08 +0700

    add seeder

---
+++ seeders/20231101033254-role-user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//Import UUID
+const { v4: uuidv4 } = require('uuid');
+
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+      await queryInterface.bulkInsert('role', [{
+        id_role : uuidv4(),
+        nama_role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id_role : uuidv4(),
+        nama_role: 'pengguna',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+      await queryInterface.bulkDelete('role', null, {})
+  }
+};
