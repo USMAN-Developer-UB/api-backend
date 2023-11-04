cb3e55f7dc0d09df02384b69f3ffb259d3d18407
Nanda hafiza <yusronnandahafiza@gmail.com>
2023-11-05 00:22:24 +0700

    add bmc pertanyaan jawaban

--- seeders/20231101033254-role-user.js
+++ seeders/20231101033254-role-user.js
@@ -1,13 +1,11 @@
-'use strict';
+"use strict";
 
 //Import UUID
-const { v4: uuidv4 } = require('uuid');
-
-
+const { v4: uuidv4 } = require("uuid");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -16,27 +14,30 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-      await queryInterface.bulkInsert('role', [{
-        id_role : uuidv4(),
-        nama_role: 'admin',
+     */
+    await queryInterface.bulkInsert("role", [
+      {
+        id_role: uuidv4(),
+        nama_role: "admin",
         createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        id_role : uuidv4(),
-        nama_role: 'pengguna',
+        updatedAt: new Date(),
+      },
+      {
+        id_role: uuidv4(),
+        nama_role: "pengguna",
         createdAt: new Date(),
-        updatedAt: new Date()
-      }])
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-      await queryInterface.bulkDelete('role', null, {})
-  }
+    await queryInterface.bulkDelete("role", null, {});
+  },
 };
--- seeders/20231104170419-pertanyaan-bmc.js
+++ seeders/20231104170419-pertanyaan-bmc.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,15 +11,161 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    await queryInterface.bulkInsert(
+      "pertanyaan",
+      [
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Siapa aja yang jadi langganan produk kamu?",
+          jenis_pertanyaan: "tentang-segmen-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Kebutuhan dan maunya apa sih yang mereka pengen?",
+          jenis_pertanyaan: "tentang-segmen-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Mereka suka nongkrong dimana dan gimana sih kelakuannya?",
+          jenis_pertanyaan: "tentang-segmen-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Ciri-ciri mereka tuh kayak gimana, dari umur sampe hobinya?",
+          jenis_pertanyaan: "tentang-segmen-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Apa sih yang bikin produk kamu keren banget buat pelanggan?",
+          jenis_pertanyaan: "tentang-tawaran-nilai",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Apa yang bedain produk atau layanan kamu dari yang lain?",
+          jenis_pertanyaan: "tentang-tawaran-nilai",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Buat pelanggan, apa untungnya pake produk atau layanan kamu?",
+          jenis_pertanyaan: "tentang-tawaran-nilai",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Gimana sih caranya kamu nyamperin pelanggan?",
+          jenis_pertanyaan: "tentang-saluran",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Jualan produk atau layanan kamu gimana caranya?",
+          jenis_pertanyaan: "tentang-saluran",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Biar pelanggan dapetin produk atau layanan kamu, caranya gimana?",
+          jenis_pertanyaan: "tentang-saluran",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Hubungan apa sih yang kamu pengen bangun sama pelanggan?",
+          jenis_pertanyaan: "tentang-hubungan-dengan-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Gimana sih caranya biar pelanggan betah terus sama kamu?",
+          jenis_pertanyaan: "tentang-hubungan-dengan-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Buat dapetin saran dari pelanggan, caranya gimana?",
+          jenis_pertanyaan: "tentang-hubungan-dengan-pelanggan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Duit kamu dapetnya dari mana aja sih?",
+          jenis_pertanyaan: "tentang-sumber-pendapatan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Berapa sih harganya kalo beli produk atau layanan kamu?",
+          jenis_pertanyaan: "tentang-sumber-pendapatan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Cara bayarnya gimana sih, macem-macem atau cuma satu?",
+          jenis_pertanyaan: "tentang-sumber-pendapatan",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Apa aja yang harus ada buat jalanin bisnis kamu? Aset itu bisa bentuk barang, kepintaran, orang, atau duit.",
+          jenis_pertanyaan: "tentang-sumber-daya-utama",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Apa aja yang harus kamu lakuin biar produk atau layanan kamu bisa jalan? Mulai dari bikin, promosi, jualan, sampe ngelayanin pelanggan.",
+          jenis_pertanyaan: "tentang-sumber-kegiatan-utama",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Siapa aja yang jadi kaki tangan utama buat bisnis kamu? Bisa supplier, penjual, atau agen deh.",
+          jenis_pertanyaan: "tentang-mitra-utama",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_pertanyaan: uuidv4(),
+          pertanyaan: "Biaya paling gede buat jalananin bisnis kamu itu apa aja? Mulai dari bikin, promosi, sampe jualan.",
+          jenis_pertanyaan: "tentang-struktur-biaya",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
