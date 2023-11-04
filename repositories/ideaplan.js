33121aa64c20f082773ad7b326aeea040c9cf2b8
Nanda hafiza <yusronnandahafiza@gmail.com>
2023-11-05 01:05:50 +0700

    add func in ideaplan

--- repositories/ideaplan.js
+++ repositories/ideaplan.js
@@ -1,97 +1,262 @@
-const {
-  ideaplan,
-  tenaga_kerja,
-  bahan_menu,
-  biaya_overhead,
-  menu,
-  Sequelize,
-  sequelize
-} = require('../models')
-const flaverr = require('flaverr');
-const { v4: uuidv4 } = require('uuid')
+const { ideaplan, tenaga_kerja, bahan_menu, biaya_overhead, menu, Sequelize, sequelize } = require("../models");
+const flaverr = require("flaverr");
+const { v4: uuidv4 } = require("uuid");
 
-const library = {}
+const library = {};
 
-module.exports = library
+module.exports = library;
 
 library.create = async (data, user, transaction) => {
   try {
-    const result = await ideaplan.create({
-      id_ideaplan : uuidv4(),
-      ...data,
-      id_pengguna: user
-    }, { transaction })
+    const result = await ideaplan.create(
+      {
+        id_ideaplan: uuidv4(),
+        ...data,
+        id_pengguna: user,
+      },
+      { transaction }
+    );
     if (!result) {
-      throw flaverr('E_FAILED', new Error('Failed to create idea plan'))
+      throw flaverr("E_FAILED", new Error("Failed to create idea plan"));
     }
     return {
       success: true,
-      result
-    }
+      result,
+    };
   } catch (err) {
     return {
       success: false,
-      err
-    }
+      err,
+    };
   }
-}
+};
 
 library.findByUser = async (user) => {
   try {
     const result = await ideaplan.findOne({
       where: {
-        id_pengguna: user
+        id_pengguna: user,
       },
-      include : [
+      include: [
         {
           model: tenaga_kerja,
-          as: 'tenaga_kerja'
+          as: "tenaga_kerja",
         },
         {
           model: menu,
-          as: 'menu',
+          as: "menu",
           include: [
             {
               model: bahan_menu,
-              as: 'bahan_menu'
-            }
-          ]
+              as: "bahan_menu",
+            },
+          ],
         },
         {
           model: biaya_overhead,
-          as: 'biaya_overhead'
-        }
-      ]
-    })
+          as: "biaya_overhead",
+        },
+      ],
+    });
     if (!result) {
-      throw flaverr('E_NOT_FOUND', new Error('Idea plan not found'))
+      throw flaverr("E_NOT_FOUND", new Error("Idea plan not found"));
     }
     return {
       success: true,
-      result
-    }
+      result,
+    };
   } catch (err) {
     return {
       success: false,
-      err
-    }
+      err,
+    };
   }
-}
+};
+
+library.findByMentor = async (user) => {
+  try {
+    const result = await ideaplan.findOne({
+      where: {
+        id_mentor: user,
+      },
+      include: [
+        {
+          model: tenaga_kerja,
+          as: "tenaga_kerja",
+        },
+        {
+          model: menu,
+          as: "menu",
+          include: [
+            {
+              model: bahan_menu,
+              as: "bahan_menu",
+            },
+          ],
+        },
+        {
+          model: biaya_overhead,
+          as: "biaya_overhead",
+        },
+      ],
+    });
+    if (!result) {
+      throw flaverr("E_NOT_FOUND", new Error("Idea plan not found"));
+    }
+    return {
+      success: true,
+      result,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      err,
+    };
+  }
+};
 
 library.findAll = async () => {
   try {
-    const result = await ideaplan.findAll()
+    const result = await ideaplan.findAll();
     if (!result) {
-      throw flaverr('E_NOT_FOUND', new Error('Idea plan not found'))
+      throw flaverr("E_NOT_FOUND", new Error("Idea plan not found"));
     }
     return {
       success: true,
-      result
-    }
+      result,
+    };
   } catch (err) {
     return {
       success: false,
-      err
-    }
+      err,
+    };
   }
-}
+};
+
+library.findByIdUser = async (id_ideaplan) => {
+  try {
+    const result = await ideaplan.findOne({
+      where: {
+        id_ideaplan: id_ideaplan,
+      },
+      include: [
+        {
+          model: tenaga_kerja,
+          as: "tenaga_kerja",
+        },
+        {
+          model: hasil_skor,
+          as: "hasil_skor",
+        },
+        {
+          model: jawaban,
+          as: "jawaban",
+          include: [
+            {
+              model: pertanyaan,
+              as: "pertanyaan",
+            },
+          ],
+        },
+        {
+          model: hpp,
+          as: "hpp",
+        },
+        {
+          model: biaya_penyusutan,
+          as: "biaya_penyusutan",
+        },
+        {
+          model: menu,
+          as: "menu",
+          include: [
+            {
+              model: bahan_menu,
+              as: "bahan_menu",
+            },
+          ],
+        },
+        {
+          model: biaya_overhead,
+          as: "biaya_overhead",
+        },
+      ],
+    });
+    if (!result) {
+      throw flaverr("E_NOT_FOUND", new Error("Idea plan not found"));
+    }
+    return {
+      success: true,
+      result,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      err,
+    };
+  }
+};
+
+library.findByIdMentor = async (id_ideaplan) => {
+  try {
+    const result = await ideaplan.findOne({
+      where: {
+        id_ideaplan: id_ideaplan,
+      },
+      include: [
+        {
+          model: tenaga_kerja,
+          as: "tenaga_kerja",
+        },
+        {
+          model: hasil_skor,
+          as: "hasil_skor",
+        },
+        {
+          model: jawaban,
+          as: "jawaban",
+          include: [
+            {
+              model: pertanyaan,
+              as: "pertanyaan",
+            },
+          ],
+        },
+        {
+          model: hpp,
+          as: "hpp",
+        },
+        {
+          model: biaya_penyusutan,
+          as: "biaya_penyusutan",
+        },
+        {
+          model: menu,
+          as: "menu",
+          include: [
+            {
+              model: bahan_menu,
+              as: "bahan_menu",
+            },
+          ],
+        },
+        {
+          model: biaya_overhead,
+          as: "biaya_overhead",
+        },
+      ],
+    });
+    if (!result) {
+      throw flaverr("E_NOT_FOUND", new Error("Idea plan not found"));
+    }
+    return {
+      success: true,
+      result,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      err,
+    };
+  }
+};
--- routes/ideaplan.js
+++ routes/ideaplan.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ideaPlanController = require('../controllers/ideaplan')
-const jwtMiddleware = require('../middleware/jwt')
+const ideaPlanController = require("../controllers/ideaplan");
+const jwtMiddleware = require("../middleware/jwt");
 
-router.post('/', jwtMiddleware.validateToken, ideaPlanController.create);
-router.get('/user', jwtMiddleware.validateToken, ideaPlanController.findByUser);
+router.post("/", jwtMiddleware.validateToken, ideaPlanController.create);
+router.get("/user", jwtMiddleware.validateToken, ideaPlanController.findByUser);
 // find All
-router.get('/mentor', jwtMiddleware.validateTokenAdmin, ideaPlanController.findByUser);
+router.get("/mentor", jwtMiddleware.validateTokenAdmin, ideaPlanController.findByMentor);
 // find By ID
-router.get('/pengguna/:id', jwtMiddleware.validateToken, ideaPlanController.findByUser);
-router.get('/mentor/:id', jwtMiddleware.validateTokenAdmin, ideaPlanController.findByUser);
-
+router.get("/pengguna/:id", jwtMiddleware.validateToken, ideaPlanController.findByIdUser);
+router.get("/mentor/:id", jwtMiddleware.validateTokenAdmin, ideaPlanController.findByIdMentor);
 
 module.exports = router;
