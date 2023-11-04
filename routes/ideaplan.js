27e21da5eb655b0b41104fc78cbd8f26880bbf7c
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-11-05 01:36:09 +0700

    add routes

--- routes/ideaplan.js
+++ routes/ideaplan.js
@@ -6,7 +6,7 @@ const jwtMiddleware = require("../middleware/jwt");
 router.post("/", jwtMiddleware.validateToken, ideaPlanController.create);
 router.get("/user", jwtMiddleware.validateToken, ideaPlanController.findByUser);
 // find All
-router.get("/mentor", jwtMiddleware.validateTokenAdmin, ideaPlanController.findByMentor);
+router.get("/mentor", jwtMiddleware.validateTokenAdmin, ideaPlanController.findAll);
 // find By ID
 router.get("/pengguna/:id", jwtMiddleware.validateToken, ideaPlanController.findById);
 router.get("/mentor/:id", jwtMiddleware.validateTokenAdmin, ideaPlanController.findById);
